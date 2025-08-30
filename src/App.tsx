96286cca3d8b83c15404a1fe5df3cbb242cd448f
JuanZ-Dev <juan.z.981611@gmail.com>
2025-08-30 18:35:14 -0500

    add the form to enter the city and country

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import styles from "./App.module.css";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <Form />
+        <div></div>
+      </div>
     </>
   );
 }
